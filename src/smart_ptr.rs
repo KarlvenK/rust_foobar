3b86d5df837a82bb735c8e997063bd5e7c86dda5
KarlvenK <karlven@outlook.com>
2022-05-02 20:14:01 +0800

    ptr_err

--- src/smart_ptr.rs
+++ src/smart_ptr.rs
@@ -237,6 +237,249 @@ mod use_cell {
     }
 }
 
+#[allow(dead_code)]
+mod ptr_err {
+    use std::cell::RefCell;
+    use std::marker::PhantomPinned;
+    use std::pin::Pin;
+    use std::ptr::NonNull;
+    use std::rc::{Rc, Weak};
+
+    fn circle_ref() {
+        #[derive(Debug)]
+        enum List {
+            Cons(i32, RefCell<Rc<List>>),
+            Nil,
+        }
+        impl List {
+            fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+                match self {
+                    List::Cons(_num, item) => Some(item),
+                    List::Nil => None,
+                }
+            }
+        }
+
+        let a = Rc::new(List::Cons(1, RefCell::new(Rc::new(List::Nil))));
+        println!("a's initial rc count = {}", Rc::strong_count(&a));
+        println!("a points to {:?}", a.tail());
+
+        //b ---> a
+        let b = Rc::new(List::Cons(2, RefCell::new(Rc::clone(&a))));
+
+        println!("after creating b, rc count of a = {}", Rc::strong_count(&a));
+        println!("b's initial rc count = {}", Rc::strong_count(&b));
+        println!("b points to {:?}", b.tail());
+
+        //       |---->
+        //      a      b
+        //       <----/
+        if let Some(link) = a.tail() {
+            *link.borrow_mut() = Rc::clone(&b);
+        }
+        println!("after changing a, b's rc count = {}", Rc::strong_count(&b));
+        println!("after changing a, a's rc count = {}", Rc::strong_count(&a));
+
+        // println!("{:?}", a.tail()); //error
+    }
+    fn try_weak() {
+        let nine = Rc::new(9);
+        let weak_nine = Rc::downgrade(&nine);
+        let string_nine = weak_nine.upgrade();
+        assert_eq!(*string_nine.unwrap(), 9);
+        drop(nine);
+        let strong_nine = weak_nine.upgrade();
+        assert_eq!(strong_nine, None);
+    }
+    fn weak_solve_circle_ref() {
+        //owner <==> gadgets
+        //owner has multiple gadgets, every single gadget has its owner
+        #[derive(Debug)]
+        struct Owner {
+            name: String,
+            gadgets: RefCell<Vec<Weak<Gadget>>>,
+        }
+        #[derive(Debug)]
+        struct Gadget {
+            id: i32,
+            owner: Rc<Owner>,
+        }
+
+        let gadget_owner = Rc::new(Owner {
+            name: "Gadget Man".to_string(),
+            gadgets: RefCell::new(Vec::new()),
+        });
+
+        let gadget1 = Rc::new(Gadget {
+            id: 1,
+            owner: gadget_owner.clone(),
+        });
+        let gadget2 = Rc::new(Gadget {
+            id: 2,
+            owner: gadget_owner.clone(),
+        });
+        gadget_owner
+            .gadgets
+            .borrow_mut()
+            .push(Rc::downgrade(&gadget1));
+        gadget_owner
+            .gadgets
+            .borrow_mut()
+            .push(Rc::downgrade(&gadget2));
+        for gadget_opt in gadget_owner.gadgets.borrow().iter() {
+            let gadget = gadget_opt.upgrade().unwrap();
+            println!("Gadget {} owned by {}", gadget.id, gadget.owner.name);
+        }
+    }
+
+    fn try_tree() {
+        #[derive(Debug)]
+        struct Node {
+            value: i32,
+            parent: RefCell<Weak<Node>>,
+            children: RefCell<Vec<Rc<Node>>>,
+        }
+        let leaf = Rc::new(Node {
+            value: 3,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(Vec::new()),
+        });
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+
+        {
+            let branch = Rc::new(Node {
+                value: 9,
+                parent: RefCell::new(Weak::new()),
+                children: RefCell::new(vec![Rc::clone(&leaf)]),
+            });
+            *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+            println!(
+                "branch strong = {}, weak = {}",
+                Rc::strong_count(&branch),
+                Rc::weak_count(&branch),
+            );
+
+            println!(
+                "leaf strong = {}, weak = {}",
+                Rc::strong_count(&leaf),
+                Rc::weak_count(&leaf),
+            );
+        }
+
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+
+    fn self_ref() {
+        // struct SelfRef<'a> {
+        //     value: String,
+        //     ptr_to_value: &'a str,
+        // }
+        // let s = "a".to_string();
+        // let v = SelfRef {
+        //     value: s,
+        //     ptr_to_value: &s,
+        // };
+        #[derive(Debug)]
+        struct WhatAboutThis<'a> {
+            name: String,
+            nickname: Option<&'a str>,
+        }
+        impl<'a> WhatAboutThis<'a> {
+            fn tie_the_knot(&'a mut self) {
+                self.nickname = Some(&self.name[..3]);
+            }
+        }
+        let mut tricky = WhatAboutThis {
+            name: "Tom Riddle".to_string(),
+            nickname: None,
+        };
+        println!("{:?}", tricky);
+        tricky.tie_the_knot();
+        // println!("{:?}", tricky);//error, borrow as immutable while it is also borrowed as mutable
+        println!("try selfRef with unsafe:\n");
+        {
+            #[derive(Debug)]
+            struct SelfRef {
+                value: String,
+                ptr_to_value: *const String, //using "*mut String", you can change value
+            }
+            impl SelfRef {
+                fn new(txt: &str) -> Self {
+                    SelfRef {
+                        value: txt.to_string(),
+                        ptr_to_value: std::ptr::null(),
+                    }
+                }
+
+                fn init(&mut self) {
+                    let self_ref: *const String = &self.value;
+                    self.ptr_to_value = self_ref;
+                }
+
+                fn value(&self) -> &str {
+                    &self.value
+                }
+
+                fn ptr_to_value(&self) -> &String {
+                    assert!(
+                        !self.ptr_to_value.is_null(),
+                        "Test::b called without Test::init being called first"
+                    );
+                    unsafe { &*(self.ptr_to_value) }
+                }
+            }
+
+            let mut t = SelfRef::new("rust");
+            t.init();
+            println!("{}, {:p}", t.value(), t.ptr_to_value);
+        }
+
+        {
+            struct Unmovable {
+                data: String,
+                slice: NonNull<String>,
+                _pin: PhantomPinned,
+            }
+            impl Unmovable {
+                fn new(data: String) -> Pin<Box<Self>> {
+                    let res = Unmovable {
+                        data,
+                        slice: NonNull::dangling(),
+                        _pin: PhantomPinned,
+                    };
+                    let mut boxed = Box::pin(res);
+                    let slice = NonNull::from(&boxed.data);
+                    unsafe {
+                        let mut_ref: Pin<&mut Self> = Pin::as_mut(&mut boxed);
+                        Pin::get_unchecked_mut(mut_ref).slice = slice;
+                    }
+                    boxed
+                }
+            }
+        }
+    }
+
+    pub fn test() {
+        circle_ref();
+        try_weak();
+        println!("-------------------------");
+        weak_solve_circle_ref();
+        try_tree();
+        println!("-------------------------");
+        self_ref(); //not common
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -251,5 +494,7 @@ mod tests {
         use_rc::test();
         println!("-----------------------------");
         use_cell::test();
+        println!("-----------------------------");
+        ptr_err::test();
     }
 }
